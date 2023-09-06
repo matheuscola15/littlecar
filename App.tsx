69b78a79f888637556beb152ebc3d7790ffb9b3f
matheuscola15 <matheuscola295@gmail.com>
2023-09-06 15:49:17 -0300

    initial

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import { useFonts, Montserrat_400Regular, Montserrat_400Regular_Italic, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import React from 'react';
-import { SafeAreaView, StatusBar, View } from 'react-native';
+import { ImageBackground, StyleSheet, SafeAreaView, StatusBar, View } from 'react-native';
 import Carrinho from './src/pages/Carrinho/index';
 
+const image = { uri: "https://docs.expo.dev/static/images/tutorial/splash.png" };
+
+
 export default function App() {
+
   const [fonteCarregada] = useFonts({
       "MontserratRegular" : Montserrat_400Regular,
       "MontserratBold" : Montserrat_700Bold,
@@ -14,10 +18,28 @@ export default function App() {
     return null;
   }
   return (
-    <View>
-      <SafeAreaView/>
-      <StatusBar/>
-      <Carrinho/>
-    </View>
+    <View style={styles.container}>
+    <ImageBackground source={image} style={styles.image}>
+    </ImageBackground>
+  </View>
   );
+  
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000a0',
+  },
+});
